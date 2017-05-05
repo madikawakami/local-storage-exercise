ad634e5cb559b2de671b98ab3e5daff0e2d3b545
Joelle Torneros <jm.torneros@gmail.com>
2017-05-05 10:45:57 -1000

    Good to go

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # local-storage-exercise
 [Intermediate] JS exercise on localstorage
+
+This is an introduction and demo to localstorage. The Web Storage API provides mechanisms by which browsers can store key/value pairs, in a much more intuitive fashion than using cookies.
+
+## Task
+Given the skeleton for adding, clearing, and deleting keys in Window.localstorage, you will use the resources below to implement these methods.
+
+## Resources
+- https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API
+- https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+- https://developer.mozilla.org/en-US/docs/Web/API/Storage
+- https://developer.mozilla.org/en-US/docs/Web/API/Location/reload
--- js/app.js
+++ js/app.js
@@ -13,44 +13,20 @@
   var handler = {
     //saves user inputs as one entry to local storage
     saveEntry: function (){
-      var inputs = document.querySelectorAll(".tcell");
-        user.id = inputs[0].value;
-        user.name = inputs[1].value;
-        user.address = inputs[2].value;
-        user.email = inputs[3].value;
-        //converts object into JSON and store in localStorage
-        localStorage.setItem("user_" + localStorage.length, JSON.stringify(user));
-        //reloads the page
-        location.reload();
+      
     },
 
     //clears user input fields on the page
     clearEntry: function(){
-      var inputs = document.querySelectorAll(".tcell");
-      for(i = 0; i< inputs.length; i++){
-        inputs[i].value = "";
-      };
+      
     },
 
     //displays user entries
     displayEntry: function(){
-      if (localStorage.length > 0){
-        var render = "<div>";
-        render += "<div id='entry_containter'>Entries:</div>";
-        for(i = 0; i < localStorage.length; i++){
-          var key = localStorage.key(i); //gets the key
-          var entry = localStorage.getItem(key); //gets data from key
-          var data = JSON.parse(entry); //parses data back into object
-          render += "<ul>";
-          render += "<li>" + data.id + "</li>";
-          render += "<li>" + data.name + "</li>";
-          render += "<li>" + data.address + "</li>";
-          render += "<li>" + data.email + "</li>";
-          render += "</ul>"
-        }
-      render += "</div>"
-      display_container.innerHTML = render;
-      }
+      
+    },
+    
+    clearEverything: function(){
     }
   };
 
@@ -61,6 +37,10 @@
   //Clear User Input Function
   var clear = document.getElementById('clear');
     clear.addEventListener('click', handler.clearEntry);
+
+  //Clear All Entries
+  var clearAll = document.getElementById('clear_storage');
+    clearAll.addEventListener('click', handler.clearEverything);
 
   window.onload = function () {
     handler.displayEntry();
